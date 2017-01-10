2b4ccc9669ac0289b4fd3d092f8c35ff42b4036b
pleycpl <h4rvey@riseup.net>
2017-01-10 03:05:00 +0300

    showing <a> tags

--- main.py
+++ main.py
@@ -15,5 +15,7 @@ ul = soup.find('ul', attrs={'id': 'channels-browse-content-grid'})
 # print(ul)  <ul id="channels-browse-content-grid"></ul>
 
 for li in ul.findAll('li', attrs={'class': 'channels-content-item yt-shelf-grid-item'}):
-    print(li.prettify())
-    
+    # print(li.prettify())
+    for a in li.findAll('a', attrs={'class': 'yt-pl-thumb-link'}):
+        print(a.prettify())
+        

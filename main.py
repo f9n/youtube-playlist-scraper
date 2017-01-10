69429d10c3eed5c93879ee70c76e4d0bc2d91661
pleycpl <h4rvey@riseup.net>
2017-01-10 03:09:15 +0300

    showing title attribute <a> tags other class attr

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ ul = soup.find('ul', attrs={'id': 'channels-browse-content-grid'})
 
 for li in ul.findAll('li', attrs={'class': 'channels-content-item yt-shelf-grid-item'}):
     # print(li.prettify())
+    for a in li.findAll('a', attrs={'class': 'yt-uix-tile-link'}):
+        # print(a.prettify())
+        print(a.get('title'))
     for a in li.findAll('a', attrs={'class': 'yt-pl-thumb-link'}):
         # print(a.prettify())
         print(a.get('href'))

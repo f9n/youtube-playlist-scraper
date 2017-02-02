eb3607c997e7a73ce12eb698cd998d492478629c
pleycpl <h4rvey@protonmail.com>
2017-02-02 00:34:55 +0000

    clean up project

--- main.py
+++ main.py
@@ -2,20 +2,78 @@ import requests
 from bs4 import BeautifulSoup
 import subprocess
 import youtube_dl
+import os
 
-youtube = "https://www.youtube.com"
-# The New Boston channel
-url = "https://www.youtube.com/user/thenewboston/playlists"
-print("Channel url : " + url)
+def getSourceCode(url):
+    """ - Getting Source Code on Website
+        http://stackoverflow.com/questions/17011357/what-is-the-difference-between-content-and-text#17011420
+        r.text is the content of the response in unicode
+        r.content is the content of the response in bytes.
+    """
+    print("Channel url: " + url)
+    sourceCode = requests.get(url).content
+    print("[+] Getting Source Code is Done")
+    return sourceCode
 
-response = requests.get(url)
-html = response.content # All source code of the web page
-soup = BeautifulSoup(html, 'html.parser')
-# print(soup.prettify())
+def getPlaylistData(sourceCode):
+    """ - Getting Data like Playlist Name and Link """
+    soup = BeautifulSoup(sourceCode, 'html.parser')
+    #<a class="yt-uix-sessionlink yt-uix-tile-link  spf-link  yt-ui-ellipsis yt-ui-ellipsis-2" dir="ltr" title="ECMAScript 6 / ES6 New Features Tutorials" aria-describedby="description-id-514268" data-sessionlink="ei=uHmSWOS8LM6p1gK7-a2oBg&amp;ved=CDgQlx4iEwjk-MvOkPDRAhXOlFUKHbt8C2Uomxw" href="/playlist?list=PL6gx4Cwl9DGBhgcpA8eTYYWg7im72LgLt">ECMAScript 6 / ES6 New Features Tutorials</a>
+    playlist = {}
+    playlistName = ""
+    playlistLink = ""
+    for a in soup.findAll('a', attrs={'class':'yt-uix-tile-link'}):
+        playlistName = a.get('title')
+        playlistName = playlistName.replace(" ", "") # Deleting space
+        playlistName = playlistName.replace("/", "_") # Change path seperator
+        playlistLink = a.get('href')
+        playlist[playlistName] = playlistLink
 
+    print("[+] Getting Playlist Name and Link is Done")
+    return playlist
+
+def displayPlaylist(playlist):
+    for name in playlist:
+        print("Name : {} , Link :{} ".format(name, playlist[name]))
+
+
+def creatDirectory(playlist, youtube):
+    """ First creating directory, then dowloading playlist each directory """
+    for name in playlist:
+        # Creatin directory
+        directoryName = "./"
+        directoryName += name
+        try:
+            # https://pymotw.com/2/subprocess/
+            subprocess.check_call("mkdir " + directoryName, shell=True)
+        except subprocess.CalledProcessError as e:
+            print(e.output)
+            continue
+        # Downloading Playlist
+        link = youtube + playlist[name]
+        options = {
+            'outtmpl' : directoryName + '/%(title)s-%(id)s.%(ext)s'
+        }
+        with youtube_dl.YoutubeDL(options) as ydl:
+            ydl.download([link])
+
+def main():
+    # Downloading all playlists each directory from TheNewBoston channel
+    youtube = "https://www.youtube.com"
+    url = "https://www.youtube.com/user/thenewboston/playlists"
+    sourceCode  = getSourceCode(url)
+    playlist    = getPlaylistData(sourceCode)
+    displayPlaylist(playlist)
+    creatDirectory(playlist, youtube)
+
+if __name__ == "__main__":
+    main()
+
+
+
+"""
 ul = soup.find('ul', attrs={'id': 'channels-browse-content-grid'})
 # print(ul)  <ul id="channels-browse-content-grid"></ul>
-
 
 playlist = {}    # Creating Dictionary
 playlist_name = ""
@@ -33,10 +91,6 @@ for li in ul.findAll('li', attrs={'class': 'channels-content-item yt-shelf-grid-
         playlist_link = a.get('href')
     playlist[playlist_name] = playlist_link
 
-#print(playlist)
-for name in playlist:
-    print("Name : {} , Link :{} ".format(name, playlist[name]))
-
 for name in playlist:
     # First creating directory, then dowloading playlist each directory
     link = youtube + playlist[name]
@@ -45,12 +99,12 @@ for name in playlist:
     subprocess.call("mkdir " + directory_name, shell=True)
     # Downloading Playlist
     options = {
-        'outtmpl' : directory_name + '/%(title)s-%(id)s.%(ext)s' 
+        'outtmpl' : directory_name + '/%(title)s-%(id)s.%(ext)s'
     }
     with youtube_dl.YoutubeDL(options) as ydl:
         ydl.download([link])
 
-"""# if you want download metadata use that code
+# if you want download metadata use that code
 options = {}
 
 with youtube_dl.YoutubeDL(options) as ydl:
